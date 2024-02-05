e2c1e2ccb6b6d2a4fe0939b5d1790b157eca3731
beteland123 <91183694+beteland123@users.noreply.github.com>
2024-02-05 16:06:50 +0300

    Add session and login and regiter to the server side

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 const pg = require('pg');
 const ejs = require('ejs');
 const app = express();
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');//to generate the secret key
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,21 +20,85 @@ const db = new pg.Pool({
   port: 5432
 });
 
-app.get('/', async (req, res) => {
+app.use(session({
+  secret: secretKey,
+  resave: false,
+  saveUninitialized: true
+}));
+// Login Page
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+// Register Page
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+//to create the user 
+app.post('/register',async(req, res)=>{
+  const {username, password} = req.body;
+  try{
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.query('INSERT INTO users (username, password) values ($1, $2)', [username, hashedPassword]);
+    res.redirect('/')
+  }catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating user');
+  }
+
+});
+// Login
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
   try {
-    const results = await db.query('SELECT * FROM todo');
-    res.render('index', { todos: results.rows });
+    const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length === 0) {
+      res.status(401).send('Invalid username or password');
+      return;
+    }
+    const user = result.rows[0];
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      res.status(401).send('Invalid username or password');
+      return;
+    }
+    req.session.userId = user.id;
+    res.redirect(`/todos`);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error retrieving todos');
+    res.status(500).send('Error logging in');
+  }
+});
+// Logout
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect('/');
+  });
+});
+
+// Todolist
+app.get('/todos', async (req, res) => {
+  const userId = req.session.userId;
+  
+  try {
+    const todosResult = await db.query('SELECT * FROM todo WHERE user_id = $1', [userId]);
+    const todos = todosResult.rows; 
+    res.render('index', { todos }); // Pass the todos as an array to the view
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving the list');
   }
 });
 
 app.post('/todos', async (req, res) => {
   const { content } = req.body;
+  const userId = req.session.userId;
   try {
-    await db.query('INSERT INTO todo (content, completed) VALUES ($1, $2)', [content, false]);
-    res.redirect('/');
+    await db.query('INSERT INTO todo (content, completed,user_id) VALUES ($1, $2, $3)', [content, false,userId]);
+    res.redirect('/todos');
   } catch (error) {
     console.error(error);
     res.status(500).send('Error creating todo');
@@ -53,7 +121,7 @@ app.post('/todos/:id/complete', async (req, res) => {
   const completed = req.body.completed;
   try {
     await db.query('UPDATE todo SET completed = $1 WHERE id = $2', [completed, todoId]);
-    res.redirect('/');
+    res.redirect('/todos');
   } catch (error) {
     console.error(error);
     res.status(500).send('Error updating todo');
@@ -63,7 +131,7 @@ app.post('/todos/:id/complete', async (req, res) => {
 app.post('/todos/clear-completed', async(re,res)=>{
   try{
     await db.query('DELETE FROM todo WHERE completed = true');
-    res.redirect('/')
+    res.redirect('/todos')
   } catch (error){
     console.error(error)
     res.status(500).send('Error to delete all')
